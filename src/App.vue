7bcef5b00684c199077b217e914bc8752c08b0a1
Thais Poentes <thais_poentes@hotmail.com>
2020-05-27 06:11:39 -0300

    estilizacao css do componente

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <!-- Single file template -->
 <template>
   <!--apresentacao do componente-->
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
 
-  <div>
-    <h1>{{titulo}}</h1>
-
-    <ul>
-      <li v-for="(foto, index) in fotos" v-bind:key="index">
-        <img :src="foto.url" :alt="foto.titulo" />
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="(foto, index) in fotos" v-bind:key="index">
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva"  :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -29,15 +33,62 @@ export default {
     //$http eh o artefato do modulo vue-resource
 
     //invocamos o servico get passando sua url, temos como retorno uma promise
-    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
 
-  //ao transformar os dados da resposta (res) em json, temos outra promise retornada
+    //ao transformar os dados da resposta (res) em json, temos outra promise retornada
     promise
-    .then(res => res.json())
-    .then(fotosRes => this.fotos = fotosRes, err => console.error(err));
+      .then(res => res.json())
+      .then(
+        fotosRes => (this.fotos = fotosRes),
+        err => console.error(err)
+      );
   }
 };
 </script>
 
 <style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+    width: 100%;
+  }
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
 </style>
