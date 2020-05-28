cb89dd9c9aa077484ac7adf95568fe17844738f9
Thais Poentes <thais_poentes@hotmail.com>
2020-05-28 07:45:59 -0300

    animacao transicao de pagina

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@
         </li>
       </ul>
     </nav>
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -34,5 +36,14 @@ export default {
   font-family: Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.4s;
 }
 </style>
