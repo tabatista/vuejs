19e6aa442fec2e7f2d280ce8e783632126e326ca
Thais Poentes <thais_poentes@hotmail.com>
2020-05-28 13:08:17 -0300

    diretiva criada: modificadores

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -9,19 +9,19 @@ Vue.directive('meu-transform', {
     //eh um hook/gancho chamado toda vez que a diretiva se prende/eh associada ao elemento do DOM
     bind(el, binding, vnode) {
         //el eh  uma referencia ao elemento do DOM o qual a diretiva foi associada
-        //o binding recupera o parametro passado para a diretiva em sua declaracao
-        //nao se pode passar mais de um parametro, mas pode passar um objeto js com propriedades
+        //o binding.value recupera o parametro passado para a diretiva em sua declaracao
+        //nao se pode passar mais de um parametro, mas pode passar um objeto js com propriedades7
+        //o binding.modifiers recupera os modificadores (declarado depois do ponto. Ex: v-meu-transform.animate sendo o animate o modificador)
         let current = 0;
         el.addEventListener('dblclick', function () {
-            let incremento = 90;
-            let animate = false;
+            let incremento = binding.value || 90;
 
-            if (binding.value) {
-                incremento = binding.value.incremento;
-                animate = binding.value.animate;
-            }
-            current += incremento;
-            if (animate)
+            //eh possivel acessar mais de um modificador
+            if (binding.modifiers.reverse) current -= incremento;
+            else current += incremento;
+
+
+            if (binding.modifiers.animate)
                 el.style.transition = 'transform 0.5s';
 
             el.style.transform = `rotate(${current}deg)`;
