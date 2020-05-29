f89db3a24d168bf4f3eccf2dfa15706311b72919
Thais Poentes <thais_poentes@hotmail.com>
2020-05-29 06:30:26 -0300

    recuperando dados do formulario: mandandando dados do formulario (@sumit) e manipulando os dados la contindos (bind:) - data binding bidirecional

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -3,21 +3,36 @@
     <h1 class="centralizado">Cadastro</h1>
     <h2 class="centralizado"></h2>
 
-    <form>
+    <form @submit.prevent="gravar()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" />
+        <input
+          id="titulo"
+          autocomplete="off"
+          @input="foto.titulo = $event.target.value"
+          :value="foto.titulo"
+        />
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" />
+        <input
+          id="url"
+          autocomplete="off"
+          @input="foto.url = $event.target.value"
+          :value="foto.url"
+        />
         <imagem-responsiva />
       </div>
 
       <div class="controle">
         <label for="descricao">DESCRIÇÃO</label>
-        <textarea id="descricao" autocomplete="off"></textarea>
+        <textarea
+          id="descricao"
+          autocomplete="off"
+          @input="foto.descricao = $event.target.value"
+          v-bind:value="foto.descricao"
+        ></textarea>
       </div>
 
       <div class="centralizado">
@@ -35,9 +50,28 @@ import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
 import Botao from "../shared/botao/Botao.vue";
 
 export default {
+  data() {
+    return {
+      foto: {
+        titulo: "",
+        url: "",
+        descricao: ""
+      }
+    };
+  },
   components: {
     "imagem-responsiva": ImagemResponsiva,
     "meu-botao": Botao
+  },
+  methods: {
+    gravar() {
+      console.log("enviar dados para a API");
+      this.foto = {
+        titulo: "",
+        url: "",
+        descricao: ""
+      };
+    }
   }
 };
 </script>
