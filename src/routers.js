d7360252ff0c5ac21d0fc4368efb40b8469268d5
Thais Poentes <thais_poentes@hotmail.com>
2020-05-29 08:48:44 -0300

    rotas nomeadas / rota default para urls invalidas / filtagrem para os itens de menu das rotas exibidos

--- src/routers.js
+++ src/routers.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home.vue';
 //uma rota eh um objeto js com duas propriedades
 //o path vazio '' equivale a localhost:3000/#/
 export const routes = [
-    { path: '', component: Home, titulo: 'Home' },
-    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
+    { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
+    { path: '/cadastro', name: 'cadastro', component: Cadastro, titulo: 'Cadastro', menu: true },
+    { path: '*', component: Home, menu: false }
 ];
